4b75bc57ad0221dca5834264950fcc3a16021190
terramine21 <kiselivish@yandex.ru>
2025-05-17 21:11:49 +0300

    Отрисовываем странички

--- djangoElectronics/trainer/admin.py
+++ djangoElectronics/trainer/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Problem, UserAnswer
 
-# Register your models here.
+@admin.register(Problem)
+class ProblemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'topic', 'unit', 'created_at')  # Поля для отображения в списке
+    list_filter = ('topic',)  # Фильтр по теме
+    search_fields = ('title', 'description')  # Поиск по названию и описанию
+
+@admin.register(UserAnswer)
+class UserAnswerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'problem', 'answer', 'is_correct', 'submitted_at')  # Поля для отображения
+    list_filter = ('is_correct', 'submitted_at')  # Фильтры
+    search_fields = ('user__username', 'problem__title')  # Поиск по пользователю и задаче
